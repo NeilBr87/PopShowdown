c6751bf43445dfa53e93413da928e3a079247dbf
NeilBrooks <neil.brooks87@gmail.com>
2023-05-11 11:21:31 +0100

    Visual update and scores

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,44 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Game from "../Game";
+import Single from "../Single";
 
 
 export default function App() {
-  document.title = 'Pop Showdown'; // Set the new document title
+  document.title = 'SOC_Pop'; // Set the new document title
+  const [gameModeChosen, setGameModeChosen] = useState(false);
+  const [cohort, setCohort] = useState(false);
+  const [single, setSingle] = useState(false);
 
+  function handleClickCohort() {
+    setGameModeChosen(true);
+    setCohort(true);
+  }
+
+  function handleClickSingle() {
+    setGameModeChosen(true);
+    setSingle(true);
+  }
 
   return (
     <div className="App">
-      <h1 className="pagetitle">Pop Showdown</h1>
+      <h1 className="pagetitle">SOC_Pop</h1>
       <h3>The hopefully fun energiser game where the cohort votes for which country has the highest population!</h3>
+      {!gameModeChosen && (
+      <div id="modebuttons">
+        <button id="cohortbutton" onClick={handleClickCohort}>Cohort mode</button>
+        <button id="solobutton" onClick={handleClickSingle}>Single player mode</button>
+      </div>)}
+      {cohort && (
       <div className="container">
         <Game />
-      </div>
+      </div>)}
       
+      {single && (
+        <div className="container">
+          <Single />
+        </div>)}
+        
       
 
       </div>
--- src/Components/Single/index.js
+++ src/Components/Single/index.js
@@ -9,11 +9,13 @@ export default function Game(props) {
   const [answerClicked, setAnswerClicked] = useState(false);
   const [comparison1, setComparison1] = useState(props.comparison);
   const [correct, setCorrect] = useState(null);
+  const [incorrect, setIncorrect] = useState(null);
   const [countryName, setCountryName] = useState();
   const [countryName1, setCountryName1] = useState(null);
   const [country1pop, setCountry1pop] = useState(null);
   const [country2pop, setCountry2pop] = useState(null);
   const [hideAnswerButton, setHideAnswerButtons] = useState(true);
+  const [score , setScore] = useState(0);
 
 
   function formatPopulation(number) {
@@ -42,18 +44,49 @@ export default function Game(props) {
       });
   }
 
-  function handleClick1() {
+  function handleClickcountry1() {
     if (country1pop > country2pop) {
-      setCorrect(`${countryName}!`);
+      setCorrect("Correct!");
+      setIncorrect(null);
+      setScore(score + 1);
       setComparison1(`${countryName} has a population of ${formatPopulation(country1pop)}, whereas ${countryName1} only has a population of ${formatPopulation(country2pop)}!`)
     }
     if (country1pop < country2pop) {
-      setCorrect(`${countryName1}!`);
+      setIncorrect("Incorrect!");
+        if (score > 0) {
+          setScore(score - 1);
+          if (score < 0) {
+            setScore(0);
+        }
+        }
+        setCorrect(null);
       setComparison1(`${countryName} only has a population of ${formatPopulation(country1pop)}, whereas ${countryName1} has a population of ${formatPopulation(country2pop)}!`)
     }
     setAnswerClicked(true);
     setHideAnswerButtons(true);
   }
+
+    function handleClickcountry2() {
+    if (country2pop > country1pop) {
+        setCorrect("Correct!");
+        setScore(score + 1);
+        setIncorrect(null);
+        setComparison1(`${countryName1} has a population of ${formatPopulation(country2pop)}, whereas ${countryName} only has a population of ${formatPopulation(country1pop)}!`)
+    }
+    if (country2pop < country1pop) {
+        setIncorrect("Incorrect!");
+        if (score > 0) {
+            setScore(score - 1);
+            if (score < 0) {
+              setScore(0);
+          }
+          }
+        setCorrect(null);
+        setComparison1(`${countryName1} only has a population of ${formatPopulation(country2pop)}, whereas ${countryName} has a population of ${formatPopulation(country1pop)}!`)
+    }
+    setAnswerClicked(true);
+    setHideAnswerButtons(true);
+    }
 
   function handleClick2(event) {
     fetch("https://restcountries.com/v3.1/all")
@@ -83,7 +116,7 @@ export default function Game(props) {
   return (
     <div className="App">
     {!buttonClicked && (
-      <button id="playbutton1" className="countrybutton" onClick={handleClick}>
+      <button id="playbutton" className="countrybutton" onClick={handleClick}>
         Play
       </button> )}
       {buttonClicked && (
@@ -112,20 +145,29 @@ export default function Game(props) {
               <h2>Which has the highest population?</h2>
               
               <div className="buttoncontainer">
-                <button className="countrybutton" onClick={handleClick1}>
-                  Reveal the Answer!
+                <button className="countrybutton" onClick={handleClickcountry1}>
+                  {countryName}
+                </button>
+                <button className="countrybutton" onClick={handleClickcountry2}>
+                    {countryName1}
                 </button>
               </div>
             </div>)};
     
           {answerClicked && (
             <div className="outcome">
-              <h3>Highest:</h3>
               <h2 id="correct">{correct}</h2>
+              <h2 id="incorrect">{incorrect}</h2>
               <p>{comparison1}</p>
               <button className="nextbutton" onClick={handleClick2}>Next round!</button>
             </div>
           )}
+
+          {buttonClicked && (
+          <div className="scorebox">
+            <p>Score: {score}</p>
+          </div>)}
+        
         </div>
     </div>
   );
